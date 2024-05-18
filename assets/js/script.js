467c2675d3b95f35e16b41119659aa0e4463eb1e
Jamie McStay <jamiemcstay@hotmail.com>
2024-05-18 12:31:29 +0000

    Display addition questioin for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,47 @@
 //Wait for the DOM To finish loading before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
-    for(let button of buttons) {
-        button.addEventListener("click", function() {
-            if(this.getAttribute("data-type") === "submit") {
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked submit!");
+                console.log
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`); 
+                runGame(gameType); 
+
             }
+
         })
+
+
     }
-    console.log("$gameType");
+
+    runGame("addition")
+
 })
+
+
 /**
  * The main game "loop" is called after the script is loaded
  * and after the users answer has been processed
  */
 
 
-function runGame() {
-let num1 = Math.floor(Math.random() * 25 + 1);
-let num2 = Math.floor(Math.random() * 25 + 1);
+function runGame(gameType) {
+    //creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25 + 1);
+    let num2 = Math.floor(Math.random() * 25 + 1);
+
+    if(gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unkown game type: ${gameType}. Aborting!`;
+    }
 
 }
 
@@ -45,7 +62,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionalQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1; 
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+"; 
 
 }
 
